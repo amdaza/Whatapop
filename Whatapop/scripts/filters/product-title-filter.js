ea98d51f4cb2167b79972ea981cd4e85850bf158
amdaza <amdazacastillo@gmail.com>
2016-06-18 19:58:26 +0200

    Title filter

---
+++ Whatapop/scripts/filters/product-title-filter.js
@@ -0,0 +1,28 @@
+
+angular
+    .module("whatapop")
+    .filter("productTitleFilter", function ($sce) {
+        return function (fullTitle) {
+            // Ensure ingredients is a string
+            fullTitle = (typeof fullTitle === "string") ? fullTitle : "";
+            // Get sring between []
+            var matches = fullTitle.match(/\[(.*?)\]/);
+
+            // Format if there is some value
+            if (matches) {
+                console.log(matches.index);
+                var platform = matches[1];
+                var title = fullTitle.substring(0, matches.index);
+                var html = `<span class="product-name-only">` +
+                        title +
+                    `</span>
+                    <br>
+                    <span class="product-platform">` +
+                        platform +
+                    `</span>`;
+                return $sce.trustAsHtml(html);
+            }
+
+            return fullTitle;
+        };
+    });
